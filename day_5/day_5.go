303376be340a86f0b1f3d64bb9f1ca0453f01664
danielenricocahall <danielenricocahall@gmail.com>
2021-12-07 17:57:40 -0500

    finished

--- day_5/day_5.go
+++ day_5/day_5.go
@@ -12,7 +12,9 @@ type Coordinate struct {
 	y int
 }
 
-func readLines(path string) []Coordinate {
+func readLines(path string,
+	computeCoordinateFunction func(x1 int, x2 int,
+		y1 int, y2 int) []Coordinate) []Coordinate {
 	file, _ := os.Open(path)
 	scanner := bufio.NewScanner(file)
 	var coordinates []Coordinate
@@ -21,31 +23,108 @@ func readLines(path string) []Coordinate {
 		coords := strings.Split(line, " -> ")
 		begin := strings.Split(coords[0], ",")
 		end := strings.Split(coords[1], ",")
-		x1, y1 := begin[0], begin[1]
-		x2, y2 := end[0], end[1]
-		if x1 == x2 {
-			x, _ := strconv.Atoi(x1)
-			y1, _ := strconv.Atoi(y1)
-			y2, _ := strconv.Atoi(y2)
-			for y := y1; y < y2; y++ {
-				coordinate := Coordinate{x: x, y: y}
-				coordinates = append(coordinates, coordinate)
-			}
-
-		} else if y1 == y2 {
-			x1, _ := strconv.Atoi(x1)
-			x2, _ := strconv.Atoi(x2)
-			y, _ := strconv.Atoi(y1)
-			for x := x1; x < x2; x++ {
-				coordinate := Coordinate{x: x, y: y}
-				coordinates = append(coordinates, coordinate)
-			}
-		}
+		x1, _ := strconv.Atoi(begin[0])
+		y1, _ := strconv.Atoi(begin[1])
+		x2, _ := strconv.Atoi(end[0])
+		y2, _ := strconv.Atoi(end[1])
+		coordinates = append(coordinates, computeCoordinateFunction(x1, x2, y1, y2)...)
 
 	}
 	return coordinates
 }
 
+func computeCoordinatesOnlyHorizontalAndVertical(
+	x1 int,
+	x2 int,
+	y1 int,
+	y2 int) []Coordinate {
+	var coordinates []Coordinate
+
+	if x1 == x2 {
+		x := x1
+		if y1 > y2 {
+			y1, y2 = y2, y1
+		}
+		for y := y1; y <= y2; y++ {
+			coordinate := Coordinate{x: x, y: y}
+			coordinates = append(coordinates, coordinate)
+		}
+
+	} else if y1 == y2 {
+		y := y1
+		if x1 > x2 {
+			x1, x2 = x2, x1
+		}
+		for x := x1; x <= x2; x++ {
+			coordinate := Coordinate{x: x, y: y}
+			coordinates = append(coordinates, coordinate)
+		}
+	}
+	return coordinates
+}
+
+func computeCoordinatesAll(
+	x1 int,
+	x2 int,
+	y1 int,
+	y2 int) []Coordinate {
+	var coordinates []Coordinate
+	if x1 != x2 && y1 != y2 {
+
+		for x, y := x1, y1; x != x2 && y != y2; {
+			coordinate := Coordinate{x: x, y: y}
+			coordinates = append(coordinates, coordinate)
+			if x1 > x2 {
+				x--
+			} else {
+				x++
+			}
+			if y1 > y2 {
+				y--
+			} else {
+				y++
+			}
+		}
+		coordinates = append(coordinates, Coordinate{x: x2, y: y2})
+	} else {
+		coordinates = append(coordinates,
+			computeCoordinatesOnlyHorizontalAndVertical(x1, x2, y1, y2)...)
+	}
+	return coordinates
+}
+func countCoordinatesOccurrences(coordinates []Coordinate) map[Coordinate]int {
+	occurrenceOfCoordinates := make(map[Coordinate]int)
+	for _, coordinate := range coordinates {
+		if _, ok := occurrenceOfCoordinates[coordinate]; ok {
+			occurrenceOfCoordinates[coordinate] += 1
+		} else {
+			occurrenceOfCoordinates[coordinate] = 1
+		}
+	}
+	return occurrenceOfCoordinates
+}
+
+func filterCoordinateOccurrences(
+	coordinateOccurrences map[Coordinate]int,
+	filterFunction func(coordinate Coordinate, occurrence int) bool) {
+	for coordinate, occurrence := range coordinateOccurrences {
+		if !filterFunction(coordinate, occurrence) {
+			delete(coordinateOccurrences, coordinate)
+		}
+	}
+}
+
 func main() {
-	println(readLines("day_5/day_5_test.txt"))
+	coordinates := readLines("day_5/day_5.txt", computeCoordinatesAll)
+	coordinateOccurrences := countCoordinatesOccurrences(coordinates)
+	println(len(coordinateOccurrences))
+	for k, v := range coordinateOccurrences {
+		println("Key: (", k.x, k.y, ") =>", v)
+	}
+	filterCoordinateOccurrences(coordinateOccurrences,
+		func(coordinate Coordinate, occurrence int) bool {
+			return occurrence > 1
+		})
+	println(len(coordinateOccurrences))
+
 }
