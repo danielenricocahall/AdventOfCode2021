ef37823ce0e51b2b79bac4f993f18dcb2e5ae4ea
danielenricocahall <danielenricocahall@gmail.com>
2021-12-08 22:17:09 -0500

    part 2 done

--- day_7/day_7.go
+++ day_7/day_7.go
@@ -56,11 +56,21 @@ func computeSumOfDifferences(positions []int, k int) int {
 	return acc
 }
 
-func computeMinimumFuel(positions []int) int {
+func computeSumOfTriangularSumDifferences(positions []int, k int) int {
+	acc := 0
+	for _, position := range positions {
+		n := computeL1(position, k)
+		acc += n * (n + 1) / 2
+	}
+	return acc
+}
+
+func computeMinimumFuel(positions []int,
+	differenceFunction func([]int, int) int) int {
 	maxPosition := computeMax(positions)
 	minFuelUsed := math.Inf(1)
 	for k := 0; k <= maxPosition; k++ {
-		fuelUsed := computeSumOfDifferences(positions, k)
+		fuelUsed := differenceFunction(positions, k)
 		if float64(fuelUsed) < minFuelUsed {
 			minFuelUsed = float64(fuelUsed)
 		}
@@ -70,5 +80,5 @@ func computeMinimumFuel(positions []int) int {
 
 func main() {
 	positions := readLines("day_7/data.txt")
-	println(computeMinimumFuel(positions))
+	println(computeMinimumFuel(positions, computeSumOfTriangularSumDifferences))
 }
