f9d150159eac63f68d702be56fe984c80e48045a
danielenricocahall <danielenricocahall@gmail.com>
2021-12-30 00:47:56 -0500

    revert

--- day_14/day_14.go
+++ day_14/day_14.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 func readLines(path string) (map[string]string, string) {
@@ -39,25 +38,13 @@ func readLines(path string) (map[string]string, string) {
 func applyPolymerRules(polymerTemplate string, rules map[string]string, steps int) string {
 	newPolymer := polymerTemplate
 	println(polymerTemplate)
-	// NCNBCHB
 	for step := 0; step < steps; step++ {
-		println("Step: " + strconv.Itoa(step))
-		var wg sync.WaitGroup
-		wg.Add(len(polymerTemplate) - 1)
-		changes := make([]string, len(polymerTemplate)-1)
+		print("STEP: " + strconv.Itoa(step))
 		for i := 0; i <= len(polymerTemplate)-2; i++ {
-			go func(i int) {
-				defer wg.Done()
-				pair := polymerTemplate[i : i+2]
-				changes[i] = rules[pair]
-			}(i)
+			pair := polymerTemplate[i : i+2]
+			newPolymer = newPolymer[:i+(i+1)] + rules[pair] + newPolymer[i+(i+1):]
 		}
-		wg.Wait()
-
-		for i, change := range changes {
-			newPolymer = newPolymer[:i+(i+1)] + change + newPolymer[i+(i+1):]
-		}
-
+		println(newPolymer)
 		polymerTemplate = newPolymer
 	}
 	return newPolymer
@@ -73,8 +60,8 @@ func countOccurrences(polymer string) {
 }
 
 func main() {
-	rules, polymerTemplate := readLines("day_14/data.txt")
-	polymer := applyPolymerRules(polymerTemplate, rules, 40)
+	rules, polymerTemplate := readLines("day_14/test.txt")
+	polymer := applyPolymerRules(polymerTemplate, rules, 10)
 	countOccurrences(polymer)
 
 }
