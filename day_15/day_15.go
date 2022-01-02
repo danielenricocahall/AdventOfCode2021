1e0babb8bfe3042ae1020ae1f3303ca1df0324f7
danielenricocahall <danielenricocahall@gmail.com>
2022-01-02 15:47:57 -0500

    functional but slow

--- day_15/day_15.go
+++ day_15/day_15.go
@@ -3,7 +3,6 @@ package main
 import (
 	. "AdventOfCode/utils"
 	"bufio"
-	"fmt"
 	"os"
 	"sort"
 	"strconv"
@@ -21,10 +20,41 @@ func readLines(path string) [][]int {
 	return results
 }
 
+func expandHorizontal(cavern *[][]int, n int) {
+	for i, row := range *cavern {
+		for j := 0; j < n-1; j++ {
+			for _, riskLevel := range row {
+				(*cavern)[i] = append((*cavern)[i], ((riskLevel+j)%9)+1)
+			}
+		}
+	}
+}
+
+func expandVertical(cavern *[][]int, n int) {
+	var newRows [][]int
+	for j := 0; j < n-1; j++ {
+		for _, row := range *cavern {
+			var newRow []int
+			for _, riskLevel := range row {
+				newRow = append(newRow, ((riskLevel+j)%9)+1)
+			}
+			newRows = append(newRows, newRow)
+		}
+		println(len(newRows))
+	}
+	*cavern = append(*cavern, newRows...)
+}
+
+func expandCavern(cavern *[][]int, n int) {
+	expandHorizontal(cavern, n)
+	expandVertical(cavern, n)
+
+}
+
 func findPathWithLowestRisk(cavern [][]int) []int {
 	var results []int
 	currentSum := 0
-	memo := make(map[int]map[int][]int)
+	memo := make(map[int]map[int]int)
 	computeNextStepWithLowestRisk(&cavern, 0, 0, currentSum, &memo, &results)
 	//bs, _ := json.Marshal(memo)
 	//fmt.Println(string(bs))
@@ -35,48 +65,54 @@ func computeNextStepWithLowestRisk(cavern *[][]int,
 	horizontalPosition int,
 	verticalPosition int,
 	currentSum int,
-	memo *map[int]map[int][]int,
-	results *[]int) []int {
+	memo *map[int]map[int]int,
+	results *[]int) int {
 	rows := len(*cavern) - 1
 	cols := len((*cavern)[0]) - 1
 	if (*memo)[verticalPosition] == nil {
 		println("Creating entry for " + strconv.Itoa(verticalPosition))
-		(*memo)[verticalPosition] = make(map[int][]int)
+		(*memo)[verticalPosition] = make(map[int]int)
 	}
-	println("(" + strconv.Itoa(verticalPosition) + "," + strconv.Itoa(horizontalPosition) + ")")
 	if verticalPosition == rows && horizontalPosition == cols {
-		(*memo)[verticalPosition][horizontalPosition] = append((*memo)[verticalPosition][horizontalPosition], []int{(*cavern)[verticalPosition][horizontalPosition] + currentSum}...)
-		*results = append(*results, currentSum+(*cavern)[verticalPosition][horizontalPosition]-(*cavern)[0][0])
-	} else if (*memo)[verticalPosition][horizontalPosition] != nil &&
-		(currentSum+(*cavern)[verticalPosition][horizontalPosition] >= (*memo)[verticalPosition][horizontalPosition][0] ||
-			(len((*memo)[verticalPosition][horizontalPosition]) == 2 &&
-				currentSum+(*cavern)[verticalPosition][horizontalPosition] >= (*memo)[verticalPosition][horizontalPosition][1])) {
-		println("Hit cache for (" + strconv.Itoa(verticalPosition) + "," + strconv.Itoa(horizontalPosition) + ")")
+		*results = append(*results, currentSum+(*cavern)[verticalPosition][horizontalPosition])
+	} else if (*memo)[verticalPosition][horizontalPosition] != 0 && currentSum+(*cavern)[verticalPosition][horizontalPosition] >= (*memo)[verticalPosition][horizontalPosition] {
+		//println("Hit cache for (" + strconv.Itoa(verticalPosition) + "," + strconv.Itoa(horizontalPosition) + ")")
 		return (*memo)[verticalPosition][horizontalPosition]
-
 	} else {
+		(*memo)[verticalPosition][horizontalPosition] = (*cavern)[verticalPosition][horizontalPosition] + currentSum
 		if verticalPosition+1 <= rows {
-			(*memo)[verticalPosition][horizontalPosition] = append((*memo)[verticalPosition][horizontalPosition],
+			(*memo)[verticalPosition+1][horizontalPosition] =
 				computeNextStepWithLowestRisk(cavern,
-					horizontalPosition, verticalPosition+1, currentSum+(*cavern)[verticalPosition][horizontalPosition], memo, results)...)
+					horizontalPosition, verticalPosition+1, currentSum+(*cavern)[verticalPosition][horizontalPosition], memo, results)
 		}
 		if horizontalPosition+1 <= cols {
-			(*memo)[verticalPosition][horizontalPosition] = append((*memo)[verticalPosition][horizontalPosition],
+			(*memo)[verticalPosition][horizontalPosition+1] =
 				computeNextStepWithLowestRisk(cavern,
-					horizontalPosition+1, verticalPosition, currentSum+(*cavern)[verticalPosition][horizontalPosition], memo, results)...)
+					horizontalPosition+1, verticalPosition, currentSum+(*cavern)[verticalPosition][horizontalPosition], memo, results)
+		}
+		if verticalPosition-1 >= 0 {
+			(*memo)[verticalPosition-1][horizontalPosition] =
+				computeNextStepWithLowestRisk(cavern,
+					horizontalPosition, verticalPosition-1, currentSum+(*cavern)[verticalPosition][horizontalPosition], memo, results)
+		}
+		if horizontalPosition-1 >= 0 {
+			(*memo)[verticalPosition][horizontalPosition-1] =
+				computeNextStepWithLowestRisk(cavern,
+					horizontalPosition-1, verticalPosition, currentSum+(*cavern)[verticalPosition][horizontalPosition], memo, results)
 		}
 	}
-	return []int{(*cavern)[verticalPosition][horizontalPosition] + currentSum}
+	return (*cavern)[verticalPosition][horizontalPosition] + currentSum
 
 }
 
 func main() {
-	cavern := readLines("day_15/test.txt")
-	fmt.Println(cavern)
+	cavern := readLines("day_15/data.txt")
+	expandCavern(&cavern, 5)
+	//fmt.Println(cavern)
 	results := findPathWithLowestRisk(cavern)
 	sort.Ints(results)
 	//fmt.Println(results)
 	println(len(results))
-	fmt.Println(results)
-	println(results[0])
+	//fmt.Println(results)
+	println(results[0] - cavern[0][0])
 }
